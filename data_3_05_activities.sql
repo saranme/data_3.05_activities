07b4eaffe1fc33a6187e36e7feef304d116269ec
saranm <s.navarrome@gmail.com>
2021-01-27 11:11:38 +0100

    update exercise

--- data_3_05_activities.sql
+++ data_3_05_activities.sql
@@ -2,7 +2,8 @@
 3.05 Activity 1
 Keep working on the bank database.
 
-Find out the average number of transactions by account. Get those accounts that have more transactions than the average.
+Find out the average number of transactions by account. 
+Get those accounts that have more transactions than the average.
 */
 SELECT AVG(n_trans)
 FROM (
@@ -29,14 +30,11 @@ Get a list of accounts from Central Bohemia using a subquery.
 Rewrite the previous as a join query.
 Discuss which method will be more efficient.
 */
-SELECT *
-FROM (
-	SELECT account_id, A3 district
-	FROM district d
-	JOIN account a
-	ON d.A1 = a.district_id
-	) AS accounts_district
-WHERE A3 = 'central Bohemia';
+SELECT DISTINCT account_id
+FROM account
+WHERE district_id IN (SELECT DISTINCT A1
+					FROM district
+					WHERE A3 = 'central Bohemia');
 
 SELECT a.account_id, d.A3 district
 FROM district d
@@ -48,13 +46,15 @@ WHERE d.A3 = 'central Bohemia';
 3.05 Activity 3
 Find the most active customer for each district in Central Bohemia.
 */
-SELECT a.account_id, COUNT(t.trans_id) n_trans
-FROM district d
-JOIN account a
-ON d.A1 = a.district_id
-JOIN trans t
-ON a.account_id = t.account_id
-WHERE d.A3 = 'central Bohemia'
+SELECT account_id, COUNT(trans_id) n_trans
+FROM trans
+WHERE account_id IN (SELECT account_id
+					FROM account
+					WHERE district_id IN (SELECT district_id
+										FROM account
+										WHERE district_id IN (SELECT A1 
+															FROM district 
+															WHERE A3 = 'central Bohemia')))
 GROUP BY 1
 ORDER BY 2 DESC
-LIMIT 1;
+LIMIT 1
